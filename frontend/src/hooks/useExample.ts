32f9b85041325617912975a9d193139d484708e6
jslim <jslim@laonroad.com>
2026-08-25 00:20:32 +0900

    ♻️ [refactor] Remove unnecessary code

--- frontend/src/hooks/useExample.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-import useAppStore from '../stores/useAppStore'
-
-export function useExample() {
-  const setInitialized = useAppStore((s) => s.setInitialized)
-  useEffect(() => {
-    setInitialized(true)
-  }, [setInitialized])
-}
